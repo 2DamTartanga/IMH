790be1d1da3397bc3ad506a13b0ade4a6fabc33a
fielleon <fielleon02@hotmail.com>
2017-01-27 13:18:05 +0100

    prueba boton menu

--- app/src/main/java/com/tartanga/dam/imhandroid/activities/Navegador.java
+++ app/src/main/java/com/tartanga/dam/imhandroid/activities/Navegador.java
@@ -2,6 +2,8 @@ package com.tartanga.dam.imhandroid.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.tartanga.dam.imhandroid.R;
 
@@ -11,5 +13,11 @@ public class Navegador extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+
+    }
+
+    public void onClickStatus(View v){
+        Toast.makeText(this,"PULSADO NUMERO 1", Toast.LENGTH_LONG).show();
     }
 }
