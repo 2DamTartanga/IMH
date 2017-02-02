231c0ff71f6d7f6a9fb524219afdee6f0e07a642
zelaitxu <zelaitxudb@gmail.com>
2017-02-02 12:18:55 +0100

    añadido listado de tipos de maquinas

--- app/src/main/java/com/tartanga/dam/imhandroid/activities/TypeMachineActivity.java
+++ app/src/main/java/com/tartanga/dam/imhandroid/activities/TypeMachineActivity.java
@@ -1,38 +1,32 @@
 package com.tartanga.dam.imhandroid.activities;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ActivityChooserView;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.tartanga.dam.imhandroid.R;
-import com.tartanga.dam.imhandroid.adaptadores.WorkOrderAdapter;
 import com.tartanga.dam.imhandroid.adaptadores.WorkZonesAdapter;
 import com.tartanga.dam.imhandroid.fragments.fragment_ZoneTotal;
 
 import java.util.List;
 
-public class WorkZonesActivity extends AppCompatActivity{
+public class TypeMachineActivity extends AppCompatActivity {
 
     List zones;
 
     private RecyclerView recycler;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager lManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_work_zones);
+        setContentView(R.layout.activity_type_machine);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.layoutFragmento1, fragment_ZoneTotal.newInstance()).commit();
-
-        recycler = (RecyclerView) findViewById(R.id.cardList);
+        recycler = (RecyclerView) findViewById(R.id.container);
         recycler.setHasFixedSize(true);
 
         lManager = new LinearLayoutManager(this);
@@ -40,14 +34,12 @@ public class WorkZonesActivity extends AppCompatActivity{
 
         adapter = new WorkZonesAdapter(zones);
         recycler.setAdapter(adapter);
-
     }
 
     public void onClickZone(View v) {
         TextView txt = (TextView) v.findViewById(R.id.tv_work_zone);
-        Intent i = new Intent(this, TypeMachineActivity.class);
+        Intent i = new Intent(this, MachinesActivity.class);
         i.putExtra("zone", txt.getText());
         startActivity(i);
     }
-
 }
