6da44cb37dc4d4651154eaf20c1cb78db5fdb1d4
zelaitxu <zelaitxudb@gmail.com>
2017-02-03 10:22:01 +0100

    no message

--- app/src/main/java/com/tartanga/dam/imhandroid/activities/InstructionsActivity.java
+++ app/src/main/java/com/tartanga/dam/imhandroid/activities/InstructionsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.tartanga.dam.imhandroid.R;
 import com.tartanga.dam.imhandroid.adaptadores.WorkOrderAdapter;
@@ -31,6 +32,10 @@ public class InstructionsActivity extends AppCompatActivity {
 
         adapter = new WorkOrderAdapter(orders);
         recycler.setAdapter(adapter);
+    }
+
+    public void onClickWorkOrder(View v){
+
     }
 
 }
