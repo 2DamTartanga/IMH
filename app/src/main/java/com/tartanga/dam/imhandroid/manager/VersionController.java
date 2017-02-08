e19df8f8a21f343e2d550c808759ea8fef6153ac
Unai Sainz de la Maza Gamboa <unaihtc70@gmail.com>
2017-02-08 19:21:37 +0100

    Refactor VersionController
    Simplify VersionController.

--- app/src/main/java/com/tartanga/dam/imhandroid/manager/VersionController.java
+++ app/src/main/java/com/tartanga/dam/imhandroid/manager/VersionController.java
@@ -1,6 +1,6 @@
 package com.tartanga.dam.imhandroid.manager;
 
-import com.tartanga.dam.imhandroid.R;
+import android.os.Build;
 
 /**
  * Created by 2dam on 06/02/2017.
@@ -9,11 +9,6 @@ import com.tartanga.dam.imhandroid.R;
 public class VersionController {
 
     public boolean olderVersions() {
-        if (android.os.Build.VERSION.SDK_INT > 19) {
-            return false;
-        }
-        else {
-            return true;
-        }
+        return Build.VERSION.SDK_INT > 19;
     }
 }
