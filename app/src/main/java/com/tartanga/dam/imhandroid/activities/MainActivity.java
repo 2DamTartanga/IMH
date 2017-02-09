4ba429bea94229d4e1463062e82538ba0dd23689
Daniel Esteban <dastebi@hotmail.com>
2017-02-09 10:03:03 +0100

    wip

--- app/src/main/java/com/tartanga/dam/imhandroid/activities/MainActivity.java
+++ app/src/main/java/com/tartanga/dam/imhandroid/activities/MainActivity.java
@@ -28,13 +28,10 @@ import com.tartanga.dam.imhandroid.manager.ThreadSender;
 import com.tartanga.dam.imhandroid.model.User;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener, MessageListener {
+        implements View.OnClickListener, MessageListener {
 
     Button btn;
     EditText username, pass;
-    Object obj = null;
-    private ListView mDrawerList;
-    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +39,6 @@ public class MainActivity extends AppCompatActivity
         StrictMode.ThreadPolicy p = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(p);
         setContentView(R.layout.fragment_login);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        setSupportActionBar(toolbar);
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);*/
 
         btn = (Button) findViewById(R.id.btn_login);
         btn.setOnClickListener(this);
@@ -64,90 +48,18 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    private void init() {
-        Manager m = Manager.getInstance();
-
-    }
-
     @Override
     public void onBackPressed() {
-        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }*/
         this.finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
-            return true;
-        }*/
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-/*
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }*/
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 
     @Override
     public void onClick(View v) {
         User u = new User(username.getText().toString(), pass.getText().toString());
-        User uServer = null;
-        //Toast.makeText(this, username.getText(), Toast.LENGTH_SHORT).show();
-        /*try {
-            Main m = new Main(Message.LOGIN, null, u);
-            uServer = (User) m.getObj();
-            Toast.makeText(this, Message.LOGIN, Toast.LENGTH_SHORT).show();
-            GlobalUser gU = new GlobalUser();
-            gU.setGlobalUser(uServer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-        //
         if (v.getId() == R.id.btn_login) {
-            ThreadSender ts = new ThreadSender(this,new Message(Message.LOGIN,null,new User("unaisainz","unaisainz")));
-            ts.execute();
             if (!username.getText().toString().isEmpty() && !pass.getText().toString().isEmpty()) {
-                //ThreadSender ts = new ThreadSender(this,new Message(Message.LOGIN,null,new User(username.getText().toString(),pass.getText().toString())));
-                //ts.execute();
+                ThreadSender ts = new ThreadSender(this,new Message(Message.LOGIN,null,u));
+                ts.execute();
             }
         }
     }
