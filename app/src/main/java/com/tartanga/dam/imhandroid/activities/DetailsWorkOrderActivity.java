4e61c648fae11215a967aac4addeb5e0275d8abe
zelaitxu <zelaitxudb@gmail.com>
2017-02-09 10:18:40 +0100

    Parte Eder, Zelai, Alexa terminada

--- app/src/main/java/com/tartanga/dam/imhandroid/activities/DetailsWorkOrderActivity.java
+++ app/src/main/java/com/tartanga/dam/imhandroid/activities/DetailsWorkOrderActivity.java
@@ -1,12 +1,17 @@
 package com.tartanga.dam.imhandroid.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.muddzdev.styleabletoastlibrary.StyleableToast;
 import com.tartanga.dam.imhandroid.R;
 import com.tartanga.dam.imhandroid.interfaces.MessageListener;
 import com.tartanga.dam.imhandroid.manager.ThreadSender;
@@ -20,12 +25,13 @@ import com.tartanga.dam.imhandroid.model.WorkOrder;
 
 import java.util.ArrayList;
 
-public class DetailsWorkOrderActivity extends AppCompatActivity implements MessageListener{
+public class DetailsWorkOrderActivity extends AppCompatActivity implements MessageListener, View.OnClickListener{
 
     private Button btnStart;
     private Button btnNext;
     private Button btnCancel;
     private String code;
+    private Button btnIns;
     private String machine, sev;
     private TextView tMachine,tBreakdown, tDescripcion, tSev, tFailure;
     private VersionController vControl = new VersionController();
@@ -55,6 +61,8 @@ public class DetailsWorkOrderActivity extends AppCompatActivity implements Messa
 
         boolean ins = getIntent().getExtras().getBoolean("Instruct");
 
+        btnIns = (Button) findViewById(R.id.btnChangePass);
+        btnIns.setOnClickListener(this);
         btnStart = (Button) findViewById(R.id.btn_start_working);
         btnNext = (Button) findViewById(R.id.btn_next);
         btnCancel = (Button) findViewById(R.id.btn_cancel_work);
@@ -71,9 +79,9 @@ public class DetailsWorkOrderActivity extends AppCompatActivity implements Messa
         }
     }
 
-    public void onClickInstructions(View v){
+    /*public void onClickInstructions(View v){
 
-    }
+    }*/
 
     public void onClickStart(View v) {
         btnStart.setVisibility(View.INVISIBLE);
@@ -113,5 +121,34 @@ public class DetailsWorkOrderActivity extends AppCompatActivity implements Messa
         tBreakdown.setText("Breakdown \n" + wOrder.getBreakdown().getSubject());
         tDescripcion.setText("Description \n" + wOrder.getBreakdown().getDescription());
         tFailure.setText("Failure type: " + wOrder.getBreakdown().getFailureType());
+    }
+
+    @Override
+    public void onClick(View view) {
+        String url = wOrder.getOthers();
+        String s = "";
+        String s2="";
+        if(url!=null && url!=""){
+            s = url.substring(0,3);
+            s2 = url.substring(0,4);
+        }
+        Log.d("INSTRUCCIONES", s2);
+        if(s2.equals("http") || s.equals("www")){
+            Log.d("INSTRUCCIONES", "ENTRA COMO WEB");
+            Intent iUrl = new Intent(Intent.ACTION_VIEW);
+            iUrl.setData(Uri.parse(url));
+            startActivity(iUrl);
+        }else if(url!=null && url!=""){
+            Intent i = new Intent(this, InstruccionesActivity.class);
+            i.putExtra("Texto", url);
+            startActivity(i);
+        }else if(url==null || url.equals("")){
+            StyleableToast t = new StyleableToast(this, getApplicationContext().getString(R.string.noInstruc), Toast.LENGTH_SHORT);
+            t.setBackgroundColor(Color.parseColor("#ff5a5f"));
+            t.setTextColor(Color.WHITE);
+            t.setIcon(R.drawable.ic_alert_login);
+            t.setMaxAlpha();
+            t.show();
+        }
     }
 }
--- app/src/main/java/com/tartanga/dam/imhandroid/activities/InstruccionesActivity.java
+++ app/src/main/java/com/tartanga/dam/imhandroid/activities/InstruccionesActivity.java
@@ -2,14 +2,22 @@ package com.tartanga.dam.imhandroid.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.tartanga.dam.imhandroid.R;
 
+import org.w3c.dom.Text;
+
 public class InstruccionesActivity extends AppCompatActivity {
+
+    TextView instruc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucciones);
+
+        instruc = (TextView) findViewById(R.id.tv_Instruccion);
+        instruc.setText(getIntent().getStringExtra("Texto"));
     }
 }
